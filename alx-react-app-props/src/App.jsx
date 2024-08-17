4172ea029bec9156406d1ccadcbe85da36815687
Emma_Tetteh <tettehe7@gmail.com>
2024-08-17 23:58:48 +0200

    new commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,6 +8,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// App.jsx
+
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
 function App() {
   const [count, setCount] = useState(0)
 
