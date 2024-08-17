42da35b7047ab5c401bc10782e43c14c668b7b66
Emma_Tetteh <tettehe7@gmail.com>
2024-08-17 23:54:57 +0200

    new commit

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,9 @@
+// UserContext.js
+
+import React from 'react';
+
+// Initialize a new context
+const UserContext = React.createContext();
+
+// Export the context to be used in other components
+export default UserContext;
