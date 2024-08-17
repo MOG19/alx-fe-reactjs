5becaff4decd6ea1de89bda6700a7fdc7f7dd3c4
Emma_Tetteh <tettehe7@gmail.com>
2024-08-17 23:26:21 +0200

    new commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Footer() {
+    return (
+        <footer style={{ color: 'pink' }}>
+               <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
