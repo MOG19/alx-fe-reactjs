f369a6541ae880d5462fe1416dfc3a1c10cb1b77
Emma_Tetteh <tettehe7@gmail.com>
2024-08-20 22:46:21 +0200

    new commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,54 @@
- // RecipeList component
- import { useRecipeStore } from './recipeStore';
+//  // RecipeList component
+//  import { useRecipeStore } from './recipeStore';
 
- const RecipeList = () => {
-   const recipes = useRecipeStore(state => state.recipes);
+//  const RecipeList = () => {
+//    const recipes = useRecipeStore(state => state.recipes);
 
-   return (
-     <div>
-       {recipes.map(recipe => (
-         <div key={recipe.id}>
-           <h3>{recipe.title}</h3>
-           <p>{recipe.description}</p>
-         </div>
-       ))}
-     </div>
-   );
- };
+//    return (
+//      <div>
+//        {recipes.map(recipe => (
+//          <div key={recipe.id}>
+//            <h3>{recipe.title}</h3>
+//            <p>{recipe.description}</p>
+//          </div>
+//        ))}
+//      </div>
+//    );
+//  };
 
- export default RecipeList;
+//  export default RecipeList;
+
+import React, { useState, useEffect } from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
+
+  const handleSearchChange = (e) => {
+    filterRecipes(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Search recipes..."
+      />
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
+
+
+["Link", "react-router-dom"]
