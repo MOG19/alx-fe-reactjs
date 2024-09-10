5b3c552ead19d60aa76fd387c98f9af85d69d8d7
Emma_Tetteh <tettehe7@gmail.com>
2024-09-10 10:35:25 +0200

    new commit

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import UserProfile from './components/UserProfile';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1 className='text-blue-500'>I am coming</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
